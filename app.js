d123abbca7b51162aa349299715bf6440906f4cf
Tobden Dorjie <07240117.sherubtse@rub.edu.bt>
2026-09-25 11:38:13 +0545

    Unit V Lab 5: REST API for restaurants, menus, orders, and stats

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Static files (CSS, client-side JS)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 // Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
@@ -27,5 +27,7 @@ app.get('/db-test', async (req, res) => {
  const result = await db.one('SELECT NOW() AS current_time');
  res.json(result);
 });
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
 
 
